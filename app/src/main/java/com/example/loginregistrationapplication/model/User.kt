d4497483a394e1d9a3253f4524d93c188694c640
Prasanth496 <saiprashanthreddy96@gmail.com>
2020-05-13 13:59:58 +0530

    [prasanth]add. created the user data class in model

--- app/src/main/java/com/example/loginregistrationapplication/model/User.kt
+++ app/src/main/java/com/example/loginregistrationapplication/model/User.kt
@@ -1,6 +1,25 @@
 package com.example.loginregistrationapplication.model
 
 
-class User(var email: String, var password: String){
+class User{
+    private lateinit var email: String
+    private lateinit var password: String
+
+    fun getEmail() : String{
+        return email
+    }
+
+    fun setEmail(email: String){
+        this.email = email
+    }
+
+    fun getPassword() : String{
+        return password
+    }
+
+    fun setPassword(password : String){
+        this.password = password
+    }
+
 
 }
