0b1182f84f673cf5c96375da0720ed018e0dbc5c
Prasanth496 <saiprashanthreddy96@gmail.com>
2020-05-13 14:16:44 +0530

    [prasanth]add. created the LoginViewModel class

--- app/src/main/java/com/example/loginregistrationapplication/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/loginregistrationapplication/viewmodel/LoginViewModel.kt
@@ -1,61 +1,16 @@
 package com.example.loginregistrationapplication.viewmodel
 
-import android.text.TextUtils
-import android.util.Patterns
-import androidx.databinding.BaseObservable
-import androidx.databinding.Bindable
+import android.content.Context
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.example.loginregistrationapplication.model.User
 
+class LoginViewModel(private var context: Context,  var user: User) : ViewModel() {
 
-class LoginViewModel : BaseObservable() {
+    public var email : MutableLiveData<String> = MutableLiveData()
+    public var password : MutableLiveData<String> = MutableLiveData()
 
-    private var user: User? = null
-    private val successMessage = "Login was successful"
-    private val errorMessage = "Email or Password not valid"
-
-    @Bindable
-    private val toastMessage: String? = null
-
-    fun getToastMessage(): String? {
-        return toastMessage
-    }
-
-    private fun setToastMessage(toastMessage: String) {
-        this.toastMessage = toastMessage
-        notifyPropertyChanged(toastMessage)
-    }
-
-
-    fun setUserEmail(email: String?) {
-        user!!.email = email!!
-        notifyPropertyChanged(userEmail)
-    }
-
-    @Bindable
-    fun getUserEmail(): String? {
-        return user!!.email
-    }
-
-    @Bindable
-    fun getUserPassword(): String {
-        return user!!.password
-    }
-
-    fun setUserPassword(password: String?) {
-        user!!.password = password!!
-        notifyPropertyChanged(userPassword)
-    }
-
-    fun LoginViewModel() {
-        user = User("", "")
-    }
-
-    fun onLoginClicked() {
-        if (isInputDataValid()) setToastMessage(successMessage) else setToastMessage(errorMessage)
-    }
-
-    fun isInputDataValid(): Boolean {
-        return !TextUtils.isEmpty(getUserEmail()) && Patterns.EMAIL_ADDRESS.matcher(getUserEmail()).matches() && getUserPassword().length > 5
+    fun onLoginClick(){
     }
 
 }
